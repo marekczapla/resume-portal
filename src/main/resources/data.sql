2a908fceccbd7c517bb2342d55a333943af0a8e0
marekczapla <106765325+marekczapla@users.noreply.github.com>
2022-11-09 11:09:03 +0100

    Added a rpofile view path

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO `user` (`id`, `user_name`, `password`, `is_active`, `roles`) VALUES
-(1, "foo", "foo", true, "USER"),
-(2, "bar", "bar", true, "USER");
+delete from user where id < 5;
+
+insert into user (id, user_name, password, is_active, roles) values
+(1, 'foo', 'foo', true, 'USER'),
+(2, 'bar', 'bar', true, 'USER');
